9cc1ad28bc4086eee79004079a3ba426b68864b6
Mohcen Benizza <mohcen.benizza.bouzid.al@iespoligonosur.org>
2025-01-31 13:01:33 +0100

    menu y eso avanzado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { EmpleadoComponent } from './empleado/empleado.component';  // Importa EmpleadoComponent
-import { AlumnadoComponent } from './alumnado/alumnado.component';  // Importa AlumnadoComponent
+import { RouterModule, RouterOutlet } from '@angular/router'; // Importa RouterModule
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { AlumnadoComponent } from './alumnado/alumnado.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,  
-  imports: [EmpleadoComponent, AlumnadoComponent],  
+  imports: [
+    RouterModule, 
+    RouterOutlet, 
+    EmpleadoComponent,
+    AlumnadoComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent
+  ],  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { AlumnadoComponent } from './alumnado/alumnado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Página de inicio
+  { path: 'home', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'empleado', component: EmpleadoComponent },
+  { path: 'alumnado', component: AlumnadoComponent }
+];
